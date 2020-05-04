6006b989a167ca7183f617d03aca04c0bbb2339f
raoul2000 <manu34@free.fr>
2020-05-04 17:24:53 +0200

    tasklist Card - ok for play action

--- src/renderer/ui/components/TaskListCard.js
+++ src/renderer/ui/components/TaskListCard.js
@@ -2,9 +2,26 @@ import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import TaskListItem from './TaskListItem';
+import { playTask, stopTask } from './actions';
 
-const TaskListCard = ({ taskList }) => {
-  console.log('TaskListCard');
+const TaskListCard = ({ taskList, onPlayTask, onStopTask }) => {
+  // usCallback is required in order to enable memoization of each
+  // taskListItem component
+  // @see https://dmitripavlutin.com/use-react-memo-wisely/#4-reactmemo-and-callback-functions
+  const handlePlay = React.useCallback((taskId) => {
+    const taskToPlay = taskList.find((task) => task.id === taskId);
+    if (taskToPlay) {
+      onPlayTask(taskToPlay);
+    }
+  }, []);
+
+  const handleStop = React.useCallback((taskId) => {
+    const taskToStop = taskList.find((task) => task.id === taskId);
+    if (taskToStop) {
+      onStopTask(taskToStop);
+    }
+  }, []);
+
   return (
     <div className="card">
       <header className="card-header">
@@ -15,7 +32,17 @@ const TaskListCard = ({ taskList }) => {
       <div className="card-content">
         <div className="content">
           {
-            taskList.map((taskItem) => <TaskListItem key={taskItem.id} task={taskItem} />)
+            taskList.map((taskItem) => (
+              <TaskListItem
+                key={taskItem.id}
+                id={taskItem.id}
+                subscribe={taskItem.subscribe}
+                result={taskItem.result}
+                error={taskItem.error}
+                onPlayTask={handlePlay}
+                onStopTask={handleStop}
+              />
+            ))
           }
         </div>
       </div>
@@ -30,13 +57,18 @@ const TaskListCard = ({ taskList }) => {
 
 TaskListCard.propTypes = {
   taskList: PropTypes.arrayOf(PropTypes.shape({
-    color: PropTypes.string,
-    fontSize: PropTypes.number
-  })).isRequired
+    id: PropTypes.string.isRequired,
+    type: PropTypes.string.isRequired
+  })).isRequired,
+  onPlayTask: PropTypes.func.isRequired,
+  onStopTask: PropTypes.func.isRequired
 };
 
 const mapStateToProps = (state) => ({
   taskList: state.taskList
 });
-
-export default connect(mapStateToProps)(TaskListCard);
+const mapDispatchToProps = {
+  onPlayTask: playTask,
+  onStopTask: stopTask
+};
+export default connect(mapStateToProps, mapDispatchToProps)(TaskListCard);
--- src/renderer/ui/components/TaskListItem.js
+++ src/renderer/ui/components/TaskListItem.js
@@ -1,42 +1,43 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import { playTask, stopTask } from './actions';
 
-const TaskListItem = ({ task, onPlayTask, onStopTask }) => {
-  const handlePlay = () => {
-    onPlayTask(task);
-  };
-  const handleStop = () => {
-    onStopTask(task);
-  };
+const TaskListItem = ({
+  id, subscribe, result, error, onPlayTask, onStopTask
+}) => {
+  console.log(`render TaskListItem ${id}`);
+
+  const handlePlay = () => onPlayTask(id);
+  const handleStop = () => onStopTask(id);
+
   return (
     <div>
       task id :
-      {task.id}
-      result = {task.result}
-      error = {task.error}
+      {id}
+      result =
+      {result}
+      error =
+      {error}
       <div className="buttons">
-        <button disabled={task.subscribe === false} className="button is-small" type="button" onClick={handlePlay}>play</button>
-        <button disabled={task.subscribe === true} className="button is-small" type="button" onClick={handleStop}>stop</button>
+        <button disabled={subscribe === false} className="button is-small" type="button" onClick={handlePlay}>play</button>
+        <button disabled={subscribe === true} className="button is-small" type="button" onClick={handleStop}>stop</button>
       </div>
     </div>
   );
 };
 
 TaskListItem.propTypes = {
-  task: PropTypes.shape({
-    id: PropTypes.string.isRequired,
-    interval: PropTypes.number.isRequired,
-    subscribe: PropTypes.bool.isRequired
-  }).isRequired,
+  id: PropTypes.string.isRequired,
+  subscribe: PropTypes.bool.isRequired,
+  result: PropTypes.number,
+  error: PropTypes.string,
   onPlayTask: PropTypes.func.isRequired,
   onStopTask: PropTypes.func.isRequired
 };
 
-const mapDispatchToProps = {
-  onPlayTask: playTask,
-  onStopTask: stopTask
+TaskListItem.defaultProps = {
+  error: null,
+  result: null
 };
 
-export default connect(null, mapDispatchToProps)(TaskListItem);
+export default React.memo(TaskListItem);
+// export default TaskListItem;
